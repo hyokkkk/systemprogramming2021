4732757a0d3e244850eee36366335593f89c8d42
hyokkkk <tahyok7123@naver.com>
2021-04-07 23:41:12 +0900

    lab2: modified alignment again

--- 2.shlab/shlab/mysplit.c
+++ 2.shlab/shlab/mysplit.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
         for (i=0; i < secs; i++){
             sleep(1);
         }
-	      exit(0);
+        exit(0);
     }
 
     /* parent waits for child to terminate */
